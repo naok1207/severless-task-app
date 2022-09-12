fc37033a042075cdd90dd791323ab0435692f238
東直希 <azumanaoki@azumanaokinoMacBook-Pro.local>
2022-09-12 12:24:24 +0900

    warning対処

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,17 +10,32 @@ import useConfig from "./hooks/useConfig";
 import MainPage from "./pages/MainPage";
 import ErrorPage from "./pages/ErrorPage";
 import { Account } from "./contexts/Account";
+import BoardPage from "./pages/BoardPage";
+import { useEffect, useState } from "react";
 
 /**
  * Our Web Application
  */
 export default function App() {
   const { app } = useConfig();
+  /**
+   * warning 対処
+   * Warning: expected server html to contain a matching div in div .
+   * https://github.com/vercel/next.js/discussions/17443
+   */
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return <></>
+
   return (
     <Account>
       <Routes>
         <Route path="/">
           <Route index element={<MainPage />} />
+          <Route path="board" element={<BoardPage />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
